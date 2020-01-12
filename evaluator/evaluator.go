816e65e6471d36337a5e0c80241143a086e9f6d4
awbear <13192872690@163.com>
2020-01-12 21:07:48 +0800

    add evaluator

---
+++ evaluator/evaluator.go
@@ -0,0 +1,44 @@
+package evaluator
+
+import (
+	"github.com/awbear/GPI/ast"
+	"github.com/awbear/GPI/object"
+)
+
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+	NULL  = &object.NULL{}
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expression
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+	}
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
